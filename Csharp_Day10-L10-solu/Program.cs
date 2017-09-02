82f7b391040eb6d6294cd332922464254db57cc7
Vinci-da-Gama <11582145@student.uts.edu.au>
2017-09-02 22:13:07 +1000

    init

---
+++ Csharp_Day10-L10-solu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Day10_L10_solu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
