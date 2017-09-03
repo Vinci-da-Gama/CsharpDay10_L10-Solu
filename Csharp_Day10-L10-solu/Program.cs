1fa41edd9cfc4d4eb7f0cd36a3e2e495859fd452
Vinci-da-Gama <11582145@student.uts.edu.au>
2017-09-03 14:13:11 +1000

    day10_entity

--- Csharp_Day10-L10-solu/Program.cs
+++ Csharp_Day10-L10-solu/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StringBuilder_DateTime;
 
 namespace Csharp_Day10_L10_solu
 {
@@ -10,6 +11,12 @@ namespace Csharp_Day10_L10_solu
     {
         static void Main(string[] args)
         {
+            StringBuilder_DateTimeClass sbdtc = new StringBuilder_DateTimeClass();
+            sbdtc.ShowSbDt();
+
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ StringBuilder_DateTime/StringBuilder_DateTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilder_DateTime
+{
+    public class StringBuilder_DateTimeClass
+    {
+        public void ShowSbDt()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 100; i++)
+            {
+                sb.Append("-- ");
+                sb.AppendLine(i.ToString()+" ");
+            }
+            Console.Write("19 {0}", sb.ToString());
+
+            string str0 = "Wrapping up our basic Entity Framework and LINQ to Entities coverage";
+
+        }
+    }
+}
